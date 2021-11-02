b2ad508383b7dc7737a026f951e30260d52a460c
Anaswara U <anaswara.u@simelabs.com>
2021-11-02 20:25:10 +0530

    First commit

---
+++ tinyproject.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class tinyproject {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("WELCOME TO SIMELABS ");
+        System.out.print("Enter your Name: ");
+        String name=scanner.next();
+    }
+}
